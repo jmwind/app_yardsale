de6194b92132d5ae662cded97f0c99e9b578b78c
Jean-Michel Lemieux <jmwind@gmail.com>
2015-05-04 13:37:15 +1000

    asset pipeline fix for script tags to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ EmbededApp::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = false
+  config.serve_static_files = true
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -47,7 +48,7 @@ EmbededApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( yard_sale.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
