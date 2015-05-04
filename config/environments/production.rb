604f8136e84da717d7a6bb2aed902f8ad61234b7
Jean-Michel Lemieux <jmwind@gmail.com>
2015-05-04 13:44:04 +1000

    another try and pre-compile for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ EmbededApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( yard_sale.js )
+  config.assets.precompile = ['*.js', '*.css', '**/*.js', '**/*.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
